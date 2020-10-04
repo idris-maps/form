f9333a2adab1b8f3eaae6f3872956f2bd8675fa0
Anders <idris.maps@gmx.ch>
2020-10-04 12:53:51 +0200

    Set initial values

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import type { Field } from './fieldTypes'
+	import { getInitialData } from './utils'
 	import RenderField from './components/Field.svelte'
 	export let fields: Field[] = []
-	let data = {}
+	let data = getInitialData(fields)
 	const setData = (key: string, value: any) => {
 		data = { ...data, [key]: value }
 		console.log(data)
@@ -14,4 +15,3 @@
 		<RenderField field={field} onChange={setData} />
 	{/each}
 </form>
-<p style="margin-top:100px">{JSON.stringify(fields)}</p>
---
+++ src/utils.ts
@@ -0,0 +1,47 @@
+import { Field, isSelect } from './fieldTypes'
+import {
+  isCheckbox,
+  isRadio,
+  isSelectOptions,
+  isRange,
+} from './fieldTypes'
+
+interface Data {
+  [key: string]: any
+}
+
+export const getInitialData = (fields: Field[]): Data =>
+  fields.reduce(
+    (result: Data, field: Field): Data => {
+      if (isCheckbox(field)) {
+        return {
+          ...result,
+          [field.property]: Boolean(field.value)
+        }
+      }
+      if (isRadio(field) || isSelect(field)) {
+        return {
+          ...result,
+          [field.property]: field.value
+            ? field.value
+            : isSelectOptions(field.options)
+              ? field.options[0].value
+              : field.options[0] 
+        }
+      }
+      if (isRange(field)) {
+        return {
+          ...result,
+          [field.property]: field.value || field.min,
+        }
+      }
+      if (field.value) {
+        return {
+          ...result,
+          [field.property]: field.value
+        }
+      }
+      return result
+    },
+    {},
+  )
